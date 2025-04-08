42c3fbc821ff0bdea820b5056a8b84756e780800
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-04-07 22:59:07 -0300

    feat: Implement category retrieval by ID and name
    This commit introduces the ability to retrieve categories by their ID or name:
    
    -   **Adds GET route `/categories/:id`:** Implements retrieval of a category by its ID. Returns a 404 error if the category is not found.
    -   **Adds GET route `/categories/query?name={name}`:** Implements retrieval of a category by its name using a query parameter. Returns a 404 error if the category is not found.

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -8,6 +8,32 @@ export const categoryRoutes = new Elysia({ prefix: "/categories" })
   .get("/", async () => {
     return categoryRepository.findAll();
   })
+  .get("/:id", async ({ params, error }) => {
+    const categoryToShow = await categoryRepository.findById(params.id);
+
+    if (!categoryToShow) {
+      return error(404, "Category not found");
+    }
+
+    return categoryToShow;
+  })
+  .get(
+    "/query",
+    async ({ query, error }) => {
+      const categoryToShow = await categoryRepository.findByName(query.name);
+
+      if (!categoryToShow) {
+        return error(404, "Category not found");
+      }
+
+      return categoryToShow;
+    },
+    {
+      query: t.Object({
+        name: t.String(),
+      }),
+    },
+  )
   .post(
     "/",
     async ({ body, set, error }) => {
